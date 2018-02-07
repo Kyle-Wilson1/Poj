60fd7d90eaf850272b3b959dad0a6ee0d064c95f
zhaochaojian <zhaochaojian@meituan.com>
2018-02-07 19:54:06 +0800

    P2533 AC

---
+++ P2533/cmake-build-debug/CMakeFiles/P2533.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P2533.dir/main.cpp.o"
+  "P2533.pdb"
+  "P2533"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P2533.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
