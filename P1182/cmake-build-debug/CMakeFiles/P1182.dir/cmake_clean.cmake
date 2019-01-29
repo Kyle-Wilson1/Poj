6a6787a9dc20143f2e3e2ac29230f1d3700ca643
kyle.wilson <kyle.wilson@foxmail.com>
2019-01-29 14:00:06 +0800

    1182

---
+++ P1182/cmake-build-debug/CMakeFiles/P1182.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1182.dir/main.cpp.o"
+  "P1182.pdb"
+  "P1182"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1182.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
