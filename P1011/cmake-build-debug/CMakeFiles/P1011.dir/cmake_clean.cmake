b9767916c3b54fa87dd29d423019f14f08da86bf
kyle.wilson <kyle.wilson@foxmail.com>
2019-01-29 20:54:00 +0800

    1011

---
+++ P1011/cmake-build-debug/CMakeFiles/P1011.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1011.dir/main.cpp.o"
+  "P1011.pdb"
+  "P1011"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1011.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
