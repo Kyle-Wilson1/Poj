16ef3f973a89ea755051a2bec11b092b2a40d35b
kyle.wilson <kyle.wilson@foxmail.com>
2019-01-30 11:24:36 +0800

    1006

---
+++ P1006/cmake-build-debug/CMakeFiles/P1006.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1006.dir/main.cpp.o"
+  "P1006.pdb"
+  "P1006"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1006.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
