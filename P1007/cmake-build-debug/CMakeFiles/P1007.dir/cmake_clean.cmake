a88575534c0186f1a707d2e34da9796e7f89989c
kyle.wilson <kyle.wilson@foxmail.com>
2019-01-30 15:12:27 +0800

    1007

---
+++ P1007/cmake-build-debug/CMakeFiles/P1007.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1007.dir/main.cpp.o"
+  "P1007.pdb"
+  "P1007"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1007.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
