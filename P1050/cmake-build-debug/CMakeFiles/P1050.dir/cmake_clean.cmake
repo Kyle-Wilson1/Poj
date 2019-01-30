c2e7323625460de576b9b8009762cc55245f9923
kyle.wilson <kyle.wilson@foxmail.com>
2019-01-30 16:14:16 +0800

    1050

---
+++ P1050/cmake-build-debug/CMakeFiles/P1050.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1050.dir/main.cpp.o"
+  "P1050.pdb"
+  "P1050"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1050.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
