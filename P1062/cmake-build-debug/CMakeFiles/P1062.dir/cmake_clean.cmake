edd6eda6571c89d83b6b32b0a34678492630de21
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-04 10:34:55 +0800

    1062

---
+++ P1062/cmake-build-debug/CMakeFiles/P1062.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1062.dir/main.cpp.o"
+  "P1062.pdb"
+  "P1062"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1062.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ P1068/cmake-build-debug/CMakeFiles/P1068.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1068.dir/main.cpp.o"
+  "P1068.pdb"
+  "P1068"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1068.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
