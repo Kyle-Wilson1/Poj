b27e9a99ec82ec0913739cbf6d3ba6280dd7fcbf
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-10 12:34:23 +0800

    1064

---
+++ P1064/cmake-build-debug/CMakeFiles/P1064.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1064.dir/main.cpp.o"
+  "P1064.pdb"
+  "P1064"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1064.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
