7b25fe54914c8567b80ffa07f773006d19ec899f
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-10 13:36:39 +0800

    1046

---
+++ P1046/cmake-build-debug/CMakeFiles/P1046.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1046.dir/main.cpp.o"
+  "P1046.pdb"
+  "P1046"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1046.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
