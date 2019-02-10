f48bb860807fe770c20eaa21bb6e0d6425a19f39
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-10 18:24:26 +0800

    1218

---
+++ P1218/cmake-build-debug/CMakeFiles/P1218.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1218.dir/main.cpp.o"
+  "P1218.pdb"
+  "P1218"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1218.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
