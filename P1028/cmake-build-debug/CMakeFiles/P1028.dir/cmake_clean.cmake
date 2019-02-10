7dd49d47d44c1969833eab214448bc43f9f6f70f
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-10 15:23:55 +0800

    1028

---
+++ P1028/cmake-build-debug/CMakeFiles/P1028.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1028.dir/main.cpp.o"
+  "P1028.pdb"
+  "P1028"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1028.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
