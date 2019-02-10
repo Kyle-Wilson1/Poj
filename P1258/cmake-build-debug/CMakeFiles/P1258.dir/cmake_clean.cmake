9fad622c7d2615d0f441cb1ddc8aaf002573e305
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-10 18:06:35 +0800

    1258

---
+++ P1258/cmake-build-debug/CMakeFiles/P1258.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1258.dir/main.cpp.o"
+  "P1258.pdb"
+  "P1258"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1258.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
