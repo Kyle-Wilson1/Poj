3be07fb9b2d4671d1cdaf9970120662e7ba3064c
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-11 22:23:58 +0800

    1401 找规律

---
+++ 1401/cmake-build-debug/CMakeFiles/1401.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1401.dir/main.cpp.o"
+  "1401.pdb"
+  "1401"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1401.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
