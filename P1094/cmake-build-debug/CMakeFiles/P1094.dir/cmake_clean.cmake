57d3283b413ee477b0a705bffd3f678dd13deb95
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-11 20:55:39 +0800

    1094 topology sort

---
+++ P1094/cmake-build-debug/CMakeFiles/P1094.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1094.dir/main.cpp.o"
+  "P1094.pdb"
+  "P1094"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1094.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
