bf84e44133a18187a7bf1f4f4902a1508d42dbae
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-11 14:46:06 +0800

    1077 cantor+bfs

---
+++ P1077/cmake-build-debug/CMakeFiles/P1077.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1077.dir/main.cpp.o"
+  "P1077.pdb"
+  "P1077"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1077.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
