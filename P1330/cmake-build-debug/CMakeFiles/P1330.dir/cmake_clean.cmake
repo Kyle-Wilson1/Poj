29abba81d68e82523f0db1767ad4a7fb14a3f1e5
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-12 20:52:02 +0800

    1330 tarjan

---
+++ P1330/cmake-build-debug/CMakeFiles/P1330.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1330.dir/main.cpp.o"
+  "P1330.pdb"
+  "P1330"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1330.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
