97bf2dce1223ed2570ee0200f8edd7bfe133f903
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-13 12:15:35 +0800

    1426 bfs 01backpack

---
+++ P1426/cmake-build-debug/CMakeFiles/P1426.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1426.dir/main.cpp.o"
+  "P1426.pdb"
+  "P1426"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1426.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
