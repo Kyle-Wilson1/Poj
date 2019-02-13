c3449ade4b058949e820ca9aeb7249d88c7ce9ce
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-13 23:06:25 +0800

    1316 simple

---
+++ P1316/cmake-build-debug/CMakeFiles/P1316.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1316.dir/main.cpp.o"
+  "P1316.pdb"
+  "P1316"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1316.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
