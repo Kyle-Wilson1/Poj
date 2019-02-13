a179d5f75bb4573f004bb6dfed9a351df1c99fc1
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-13 13:54:40 +0800

    1459 ek

---
+++ P1459/cmake-build-debug/CMakeFiles/P1459.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1459.dir/main.cpp.o"
+  "P1459.pdb"
+  "P1459"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1459.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
