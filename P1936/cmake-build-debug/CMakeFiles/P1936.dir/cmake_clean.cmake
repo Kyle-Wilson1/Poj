edba0095cc05f684e4766386c973408492b8b223
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-13 15:39:43 +0800

    1936 string

---
+++ P1936/cmake-build-debug/CMakeFiles/P1936.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1936.dir/main.cpp.o"
+  "P1936.pdb"
+  "P1936"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1936.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
