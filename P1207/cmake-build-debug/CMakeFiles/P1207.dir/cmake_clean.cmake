02f63d82b3589f1cd750c99f1df8df69bc8ff14d
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-14 16:26:53 +0800

    1207 rmq  simple

---
+++ P1207/cmake-build-debug/CMakeFiles/P1207.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1207.dir/main.cpp.o"
+  "P1207.pdb"
+  "P1207"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1207.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
