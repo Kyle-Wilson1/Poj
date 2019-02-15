c2e7d04df0370dd98f332195ad40820be1127133
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-15 21:21:20 +0800

    1251 prim

---
+++ P1251/cmake-build-debug/CMakeFiles/P1251.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1251.dir/main.cpp.o"
+  "P1251.pdb"
+  "P1251"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1251.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
