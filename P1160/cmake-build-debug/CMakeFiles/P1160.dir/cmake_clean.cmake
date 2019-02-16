7401958efaf899884bbb4fccdb0dc15cda3c3730
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-16 12:01:39 +0800

    1160 dp distance, median

---
+++ P1160/cmake-build-debug/CMakeFiles/P1160.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1160.dir/main.cpp.o"
+  "P1160.pdb"
+  "P1160"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1160.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
