d5712530c86ea3b9c4aeccd8d21a8d68a5944a4b
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-18 19:11:50 +0800

    3980 4011

---
+++ P3980/cmake-build-debug/CMakeFiles/P3980.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P3980.dir/main.cpp.o"
+  "P3980.pdb"
+  "P3980"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P3980.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ P4011/cmake-build-debug/CMakeFiles/P4011.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P4011.dir/main.cpp.o"
+  "P4011.pdb"
+  "P4011"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P4011.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
