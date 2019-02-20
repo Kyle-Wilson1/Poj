3e11b4fb462b557eacfa4f0dde5a223da97f43aa
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-20 13:14:35 +0800

    1083

---
+++ P1083/cmake-build-debug/CMakeFiles/P1083.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1083.dir/main.cpp.o"
+  "P1083.pdb"
+  "P1083"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1083.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
