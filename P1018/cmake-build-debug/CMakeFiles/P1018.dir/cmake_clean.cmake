2bb82b0efc2b24423f27aba33af391dce84f60c0
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-22 00:16:48 +0800

    1018 枚举+贪心

---
+++ P1018/cmake-build-debug/CMakeFiles/P1018.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1018.dir/main.cpp.o"
+  "P1018.pdb"
+  "P1018"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1018.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
