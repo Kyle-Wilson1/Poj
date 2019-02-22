2d718a959228f89fafeb41b0a1228523f7c08fe5
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-22 22:38:38 +0800

    1035 字符串处理

---
+++ P1035/cmake-build-debug/CMakeFiles/P1035.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1035.dir/main.cpp.o"
+  "P1035.pdb"
+  "P1035"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1035.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
