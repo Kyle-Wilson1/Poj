00f07abb7c92a32e4a71039b329e69f1817319af
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-23 19:26:47 +0800

    1045 数学公式题

---
+++ P1045/cmake-build-debug/CMakeFiles/P1045.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1045.dir/main.cpp.o"
+  "P1045.pdb"
+  "P1045"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1045.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
