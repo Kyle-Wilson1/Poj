b0265523bb1f5874cb5c4653caebc11395ec3855
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-24 20:25:15 +0800

    1195 树状数组

---
+++ P1195/cmake-build-debug/CMakeFiles/P1195.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1195.dir/main.cpp.o"
+  "P1195.pdb"
+  "P1195"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1195.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
