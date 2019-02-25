8a3c7c994277f5a6a4ecad43c5344f57eccc870d
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-25 19:16:53 +0800

    1298 3749 字符串处理

---
+++ P1298/cmake-build-debug/CMakeFiles/P1298.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1298.dir/main.cpp.o"
+  "P1298.pdb"
+  "P1298"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1298.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
