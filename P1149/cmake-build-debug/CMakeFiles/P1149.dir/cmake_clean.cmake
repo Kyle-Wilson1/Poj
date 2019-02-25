2f55e9218239c8c26338e5c0efd64e1d77e14fa5
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-25 17:02:44 +0800

    1149 dinic

---
+++ P1149/cmake-build-debug/CMakeFiles/P1149.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1149.dir/main.cpp.o"
+  "P1149.pdb"
+  "P1149"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1149.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
