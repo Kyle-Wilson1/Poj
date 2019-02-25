2d0c3e1c497aed4cde728370951944d246181874
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-25 18:19:56 +0800

    1276 多重背包

---
+++ P1276/cmake-build-debug/CMakeFiles/P1276.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1276.dir/main.cpp.o"
+  "P1276.pdb"
+  "P1276"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1276.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
