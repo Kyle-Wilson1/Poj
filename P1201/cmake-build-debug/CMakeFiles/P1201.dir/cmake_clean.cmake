b72d4ef771ad2886b1359a0252abe142288a5274
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-26 21:15:15 +0800

    1201 差分约束

---
+++ P1201/cmake-build-debug/CMakeFiles/P1201.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1201.dir/main.cpp.o"
+  "P1201.pdb"
+  "P1201"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1201.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
