e54a258ab54a43cc170c7f8c1d0bfdf11cf892f3
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-27 00:04:11 +0800

    1308 并查集

---
+++ P1308/cmake-build-debug/CMakeFiles/P1308.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1308.dir/main.cpp.o"
+  "P1308.pdb"
+  "P1308"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1308.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
