57b253799305adfbed57ea5ccdeee27a687ab244
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-27 14:35:35 +0800

    1423 斯特林公式

---
+++ P1423/cmake-build-debug/CMakeFiles/P1423.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1423.dir/main.cpp.o"
+  "P1423.pdb"
+  "P1423"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1423.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
