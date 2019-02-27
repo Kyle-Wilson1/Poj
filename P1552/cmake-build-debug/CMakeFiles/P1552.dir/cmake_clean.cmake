9c2d12ab8e529c0e7745ac45f8bddac964d87f61
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-27 20:27:57 +0800

    1552 模拟

---
+++ P1552/cmake-build-debug/CMakeFiles/P1552.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1552.dir/main.cpp.o"
+  "P1552.pdb"
+  "P1552"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1552.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
