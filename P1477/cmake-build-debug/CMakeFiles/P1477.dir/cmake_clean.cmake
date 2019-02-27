a9b1787f8576e5a0853f546f41797a5a338e568e
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-27 13:15:10 +0800

    模拟

---
+++ P1477/cmake-build-debug/CMakeFiles/P1477.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1477.dir/main.cpp.o"
+  "P1477.pdb"
+  "P1477"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1477.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
