9dcf1816d592d14ac3e76cbb7014072225ab1a24
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-27 20:10:03 +0800

    1503 高精度加法

---
+++ P1503/cmake-build-debug/CMakeFiles/P1503.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1503.dir/main.cpp.o"
+  "P1503.pdb"
+  "P1503"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1503.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
