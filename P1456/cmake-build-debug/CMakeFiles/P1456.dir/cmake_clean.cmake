6812b7668220213f80aeb67e074e40dbab423480
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-27 17:11:06 +0800

    1456 并查集+贪心

---
+++ P1456/cmake-build-debug/CMakeFiles/P1456.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1456.dir/main.cpp.o"
+  "P1456.pdb"
+  "P1456"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1456.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
