57a278c55228d860c6bf34b3ae51a84c7156197f
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-27 11:44:22 +0800

    1338 数学题

---
+++ P1338/cmake-build-debug/CMakeFiles/P1338.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1338.dir/main.cpp.o"
+  "P1338.pdb"
+  "P1338"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1338.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
