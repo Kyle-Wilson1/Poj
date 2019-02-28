2a58af036094204db752010d44de7b86ddccfd30
kyle.wilson <kyle.wilson@foxmail.com>
2019-02-28 16:53:59 +0800

    1504 高精度加法

---
+++ P1504/cmake-build-debug/CMakeFiles/P1504.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1504.dir/main.cpp.o"
+  "P1504.pdb"
+  "P1504"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1504.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
